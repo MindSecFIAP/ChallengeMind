1f0217529d2bb88267c415be1d21697b5e50284e
andrebeolchi <rm84163@fiap.com.br>
2021-10-13 23:26:20 -0300

    Criando props Children para Sidebar

--- src/pages/chat/user.js
+++ src/pages/chat/user.js
@@ -2,6 +2,16 @@ import React from 'react'
 import styled from "styled-components";
 import Chat from '../../components/Chat'
 import SideBar from '../../components/UserSideBar'
+import {SideBarWrapper,
+    SideBarInput,
+    ChatWrapper,
+    ChatItem,
+    Avatar,
+    Name,
+    Message,
+    ItemWrapper,
+    InputWrapper } from '../../components/UserSideBar/SideBarElements'
+import { UserData } from '../../components/UserSideBar/UserData'
 
 export const Container = styled.div`
     display: flex;
@@ -13,7 +23,26 @@ const ChatUser = () => {
     return (
         <>
             <Container>
-                <SideBar/>
+                <SideBar>
+                    <InputWrapper>
+                        <SideBarInput/>
+                    </InputWrapper>
+                    <SideBarWrapper>
+                        <ChatWrapper>                          
+                            {UserData.map((data) => {
+                            return (
+                                <ChatItem isOpen={false}>
+                                    <Avatar src={data.userAvatar}/>
+                                    <ItemWrapper>
+                                        <Name>{data.userName}</Name>
+                                        <Message>{data.userLastMessage}</Message>
+                                        </ItemWrapper>
+                                </ChatItem>
+                            )})}
+                        </ChatWrapper>
+                    </SideBarWrapper>
+                </SideBar>
+
                 <Chat/>
             </Container>
         </>
