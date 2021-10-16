d96685d45c974452cedc92afe174f6c868aa13c1
Leandroyyy <leandro.cavallari123@gmail.com>
2021-10-16 20:42:47 -0300

    finalizado o post dos forms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import FormLogin from '../components/FormLogin'
 import NavBar from '../components/NavBar'
 
 const LoginPage = () => {
+
+    useEffect(()=>{
+        axios.get("/cadastro").then((response) => {
+            console.log(response.data);
+          });
+    },[])  
+
     return (
         <>
             <NavBar/>
